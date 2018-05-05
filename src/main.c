63292da2e7c9693db2086bf6d58c595c1b1c71ae
Fredrik <simson@thesimson.net>
2018-05-05 22:18:28 +0900

    Added hscroll and vscroll and button input

--- src/main.c
+++ src/main.c
@@ -8,9 +8,8 @@ Fredrik Simonsson
 #include "diag/Trace.h"
 
 #include "Timer.h"
-
-void showline(int line,uint8_t *bitmap ){
 #define WAITTICK 1
+void showline(int line,uint8_t *bitmap ){
 	if (line == 7 ){
 		  // Line 7 is on GPIO C bit 1
 	      GPIOC->ODR = 2;
@@ -22,6 +21,21 @@ void showline(int line,uint8_t *bitmap ){
 	      GPIOA->ODR = 0 + *bitmap;
 	      timer_sleep( WAITTICK);
 }
+
+void showline_hscroll(int line,int shift,uint8_t *bitmap0, uint8_t *bitmap1 ){
+	if (line == 7 ){
+		  // Line 7 is on GPIO C bit 1
+	      GPIOC->ODR = 2;
+	      GPIOB->ODR = 0;
+	}else {
+	      GPIOC->ODR = 0;
+	      GPIOB->ODR = 1<<line;
+	}
+	uint32_t bitmap = (bitmap0[line]*256)+bitmap1[line];
+	bitmap = bitmap >> ( 8 - (7&shift));
+	GPIOA->ODR = bitmap;
+	timer_sleep( WAITTICK);
+}
 int main(int argc, char* argv[]) {
 #define C_HEART 0
 #define C_SHI 1
@@ -29,13 +43,17 @@ int main(int argc, char* argv[]) {
 #define C_N   3
 #define C_SO  4
 #define C_STAR 5
-  int msg[] = {C_SHI,C_MO,C_N,C_SO,C_N,C_HEART,C_STAR,C_HEART,-1};
+#define C_CHESS 6
+#define C_DOT 7
+  int msg[] = {C_CHESS,C_DOT,C_CHESS,C_DOT,C_HEART,C_DOT,C_HEART,C_SHI,C_MO,C_N,C_SO,C_N,C_HEART,C_STAR,C_HEART,C_DOT,-1};
   uint8_t bitmap[] = {0x66,0x99,0x81,0xc3,0xc3,0x66,0x3c,0x18,  //Heart
 		              0x60,0x02,0x62,0x04,0x08,0x10,0x60,0x00,  //katakana shi
 					  0x3c,0x10,0x10,0x7e,0x10,0x10,0x0c,0x00,  //katakana mo
 		              0x00,0x00,0x60,0x64,0x04,0x04,0x78,0x00,  //katakana n
 		              0x00,0x44,0x44,0x24,0x04,0x18,0x00,0x00,  //katakana so
-					  0x00,0x00,0x66,0x18,0x18,0x66,0x00,0x00	//star
+					  0x00,0x00,0x66,0x18,0x18,0x66,0x00,0x00,	//star
+					  0xaa,0x55,0xaa,0x55,0xaa,0x55,0xaa,0x55,  //chess
+					  0x00,0x00,0x00,0x00,0x08,0x00,0x00,0x00   //dot
                      };
 
   timer_start();
@@ -55,19 +73,34 @@ int main(int argc, char* argv[]) {
   // 11 Analogue
   GPIOA->MODER = (GPIOA->MODER & 0xFFFF0000) | 0x00005555 ;
   GPIOB->MODER = (GPIOB->MODER & 0xFFFF0000) | 0x00005555 ;
-  GPIOC->MODER = (GPIOC->MODER & 0xFFFFFFF3) | 0x00000004 ;
+  GPIOC->MODER = (GPIOC->MODER & 0xF3FFFFF3) | 0x00000004 ;
 
   volatile int i=0;
    // Infinite loop
    while(1){
 
       for(int vbl=0;vbl<8;vbl++){
-    	  uint32_t cindex=((i+vbl)&~7)/8;
-    	  int line = ((i+vbl)&7);
-    	  if(msg[cindex] == -1 ){
-    		  cindex=0;
+          uint32_t cindex=((i+vbl)&~7)/8;
+          if(msg[cindex] == -1 ){
+                  cindex=0;
+          }
+          uint32_t cindex0=((i)&~7)/8;
+          uint32_t cindex1=((i+8)&~7)/8;
+
+    	  if(msg[cindex0] == -1 ){
+    		  cindex0=0;
     	  }
-    	  showline(vbl&7,&bitmap[(8*msg[cindex])+line]);
+    	  if(msg[cindex1] == -1 ){
+    		  cindex1=0;
+    	  }
+
+    	  if (GPIOC->IDR & 0x2000){
+              int line = ((i+vbl)&7);
+              showline(vbl&7,&bitmap[(8*msg[cindex])+line]);
+    	  }else{
+    	  	  int line = ((i)&7);
+             showline_hscroll(vbl,line,&bitmap[(8*msg[cindex0])],&bitmap[(8*msg[cindex1])] );
+    	  }
 	  }
       x++;
       if( x== 20){
